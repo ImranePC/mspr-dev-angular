5e930fa52353b7a654d8413e9e98eab7fb535cb2
ImranePC <imranelevrai@gmail.com>
2021-08-22 22:25:11 +0200

    capacitor init and android install

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mspr-dev-angular',
+  webDir: 'dist/mspr-dev-angular',
+  bundledWebRuntime: false
+};
+
+export default config;
